aa775cc6b08b3a34011aa0a5dcd30058ccf19940
Diana-Nicoleta Sirbu <118627418+Hbax112@users.noreply.github.com>
2024-10-24 21:57:49 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+print("minesweeper12387a8sg")
+
+import pygame
+
+
+a = int(input("insert a range"))
+
+for i in range(a):
+    print("i love minesweeper")
+
+print("MinesweeperTheBestGame")
+
+
+"""Daca citesti asta esti un / o dragut/a"""
